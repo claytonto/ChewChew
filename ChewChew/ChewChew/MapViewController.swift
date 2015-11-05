afeacbab94a7c66feb66fcfdeb2b697212c399c9
Clayton To <clayto@students.pitzer.edu>
2015-11-05 10:09:19 -0800

    Implemented reverse geocoding function in map view controller
    yet to be tested, need geocoding api to be enabled in the developers
    console

--- ChewChew/ChewChew/MapViewController.swift
+++ ChewChew/ChewChew/MapViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import GoogleMaps
+import SwiftyJSON
 
 class MapViewController: UIViewController {
 
@@ -19,22 +20,18 @@ class MapViewController: UIViewController {
     var locationCoordinates: [Double]!
     var locationName: String!
     
+    var stationAddress: String!
+    var locationAddress: String!
+    
+    var stationPosition: CLLocationCoordinate2D!
+    var locationPosition: CLLocationCoordinate2D!
+    
     override func  viewDidLoad() {
         // making the positions for the station and the marker
-        let locationPosition = CLLocationCoordinate2DMake(locationCoordinates[0], locationCoordinates[1])
-        let stationPosition = CLLocationCoordinate2DMake(stationCoordinates[0], stationCoordinates[1])
-        //making the markers for the station and the location
-        let locationMarker = GMSMarker(position: locationPosition)
-        let stationMarker = GMSMarker(position : stationPosition)
-        locationMarker.title = locationName
-        stationMarker.title = stationName
-        // cneter the map around the station
-        let camera: GMSCameraPosition = GMSCameraPosition.cameraWithLatitude(stationCoordinates[0], longitude: stationCoordinates[1], zoom: 15.0)
-        Map.camera = camera
-        //placing the markers on the map
-        stationMarker.map = Map
-        locationMarker.map = Map
-        
+        stationPosition = CLLocationCoordinate2DMake(stationCoordinates[0], stationCoordinates[1])
+        locationPosition = CLLocationCoordinate2DMake(locationCoordinates[0], locationCoordinates[1])
+        placeMarkersOnMap()
+        reverseGeocodeCoordinates(stationCoordinates[0],long: stationCoordinates[1])
 
     }
 
@@ -44,6 +41,32 @@ class MapViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func placeMarkersOnMap (){
+        //creating markers to be placed
+        let locationMarker = GMSMarker(position: locationPosition)
+        let stationMarker = GMSMarker(position : stationPosition)
+        locationMarker.title = locationName
+        stationMarker.title = stationName
+        // center the map around the station
+        let camera: GMSCameraPosition = GMSCameraPosition.cameraWithLatitude(stationCoordinates[0], longitude: stationCoordinates[1], zoom: 12.0)
+        Map.camera = camera
+        //placing the markers on the map
+        stationMarker.map = Map
+        locationMarker.map = Map
+    }
+    
+    func reverseGeocodeCoordinates(lat :Double, long :Double){
+        let url: NSString = "https://maps.googleapis.com/maps/api/geocode/json?latlng=\(lat),\(long)&key=AIzaSyByM6vaj-YYY1dt3qd99JS7NrA6KmkRY5Q"
+        let urlString: NSString = url.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!
+        let searchURL: NSURL = NSURL(string:urlString as String)!
+        
+        let jsonData = NSData(contentsOfURL: searchURL)
+        let json = JSON(data: jsonData!)
+        print(json)
+
+    }
+    
+
 
     /*
     // MARK: - Navigation
